9d0b3ba63faca5898b3bdd1e21cf998632f1818b
CyanWoods <wyz597491644@gmail.com>
2021-02-23 21:39:26 +0800

    First commit

---
+++ 1-1.c
@@ -0,0 +1,23 @@
+//一个最简单的分支代码
+
+
+#include <stdio.h>
+int main()
+{
+	int no;
+	int ans=7;
+
+	printf("猜一个一到九之间的整数\n\n");
+	printf("你猜是多少呢？:");
+	scanf("%d",&no);
+	
+	if (no>ans)
+		printf("太大了，小一点。\n");
+	else if (no<ans)
+		printf("太小了，大一点。\n");
+	else
+		printf("你猜对了\n");
+
+	return 0;
+}
+	
