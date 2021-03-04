5cb94ae7de7f13dc111b39e731df525d1131d681
CyanWoods <wyz597491644@gmail.com>
2021-03-04 16:54:57 +0800

    add file

--- Second/Chap2/2-4.c
+++ Second/Chap2/2-4.c
@@ -2,5 +2,14 @@
 
 int main(void)
 {
+    printf("关于字符串常量与字符常量\n");
+   
+    putchar('"');
+    printf("双引号\n");
+    printf("用双引号括起来的\"ABC\"是字符串常量。\n");
 
+    putchar('\'');
+    printf("单引号\n");
+    printf("用单引号括起来的'ABC'是字符串常量。\n");//同时注意这里的单引号双引号的引出方法；
+    return 0;
 }
