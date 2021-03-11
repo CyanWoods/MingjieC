7db4325d4049d5956ad2298bbae21baca7ebb5d7
CyanWoods <wyz597491644@gmail.com>
2021-03-11 17:16:20 +0800

    .

--- In-book-code/Chap3/3-4.c
+++ In-book-code/Chap3/3-4.c
@@ -10,7 +10,7 @@ void strdump(const char *s)
 		unsigned char x = (unsigned char)*s;
 
 		printf("%c  ", isprint(x) ? x : ' ');	
-		printf("%0*  ", (CHAR_BIT + 3) / 4);
+		printf("%0*X  ", (CHAR_BIT + 3) / 4, x);
 		for (i = CHAR_BIT - 1; i >= 0; i--)		
 			putchar(((x >> i) & 1U) ? '1' : '0');
 		putchar('\n');
