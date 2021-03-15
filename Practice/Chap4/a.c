e508acf18de1b33d8bf62999b6e1e79733f93a66
CyanWoods <wyz597491644@gmail.com>
2021-03-15 10:09:50 +0800

    add files

--- Practice/Chap4/a.c
+++ Practice/Chap4/a.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+void gen4dig(int x[])
+{
+    srand(time(NULL));
+    int a , b;
+    for (a=1,a<=4,a++)
+        {
+
