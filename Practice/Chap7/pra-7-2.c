93bb76e1425cae62329093299b6404793555f4bc
CyanWoods <wyz597491644@gmail.com>
2021-03-22 21:04:46 +0800

    some practice code

---
+++ Practice/Chap7/pra-7-2.c
@@ -0,0 +1,78 @@
+/* 寻找幸运数字训练（其二：数字随机排列）*/
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "getputch.h"   //这玩意儿在类unix环境下使用gcc编译的时候要添加-lcurses参数来调用里面的curses.h头文件
+
+#define MAX_STAGE	2			/* 挑战次数 */
+#define swap(type, x, y)	do { type t = x; x = y; y = t; } while (0)
+
+int main(void)
+{
+	int i, j, stage;
+	int dgt[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int a[9];
+	double jikan;				/* 时间 */
+	clock_t start, end;			/* 开始时间和结束时间 */
+
+	init_getputch();
+	srand(time(NULL));			/* 设定随机数的种子 */
+
+	printf("请输入缺少的数字。\n");
+
+	start = clock();
+	for (stage = 0; stage < MAX_STAGE; stage++) {
+		int x = rand() % 9;		/* 生成随机数0~8 */
+		char no;					/* 读取的值 */
+
+		i = j = 0;
+		while (i < 10) {			/* 复制时跳过dgt[x] */
+			if (i != x)
+				a[j++] = dgt[i];
+			i++;
+		}
+
+		for (i = 8; i > 0; i--)    /* 重新排列数组a */
+		{			
+			int j = rand() % (i + 1);
+			if (i != j)
+				swap(int, a[i], a[j]);
+		}
+
+		for (i = 0; i < 9; i++)		/* 显示所有元素 */
+			printf("%d ", a[i]);
+		printf(":\n");
+
+		do {
+			no = getch( );
+			if (isprint(no)) {			/* 如果能显示的话 */
+				putch(no);				/* 显示按下的键 */
+				if (no != dgt[x] + '0')	/* 如果回答错误 */
+					putch('\b');		/* 把光标往前退一格 */
+				else
+					printf("\n");		/* 换行 */
+				fflush(stdout);
+			}
+		} while (no != dgt[x] + '0');		/* 循环到玩家输入正确答案为止 */
+	}
+	end = clock();
+
+	jikan = (double)(end - start) / CLOCKS_PER_SEC*1000; 		//针对unix修改了一下宏
+	
+	term_getputch();
+
+	printf("\n");
+	printf("用时%.1f秒。\n", jikan);
+
+	if (jikan > 25.0)
+		printf("反应太慢了。\n");
+	else if (jikan > 20.0)
+		printf("反应有点慢呀。\n");
+	else if (jikan > 17.0)
+		printf("反应还行吧。\n");
+	else
+		printf("反应真快啊。\n");
+
+	return 0;
+}
